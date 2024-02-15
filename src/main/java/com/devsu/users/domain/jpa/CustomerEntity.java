503477e0a0316c19e74f0db42a29b1d22594e9ae
Stalyn-1994 <iguairac@pichincha.com>
2024-02-14 23:50:17 -0500

    [feature/manage-users] configurate entity customer and person

--- src/main/java/com/devsu/users/domain/jpa/CustomerEntity.java
+++ src/main/java/com/devsu/users/domain/jpa/CustomerEntity.java
@@ -2,35 +2,18 @@ package com.devsu.users.domain.jpa;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "customer", schema = "users")
-public class CustomerEntity {
+public class CustomerEntity extends PersonEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  Long id;
-  @Column(nullable = false, length = 255)
-  private String name;
-  @Column(length = 50)
-  private String gender;
-  private Integer age;
-  @Column(length = 100)
-  private String identification;
-  @Column(length = 255)
-  private String address;
-  @Column(length = 50)
-  private String cellphone;
   @Column(length = 255)
   private String password;
   @Column(length = 50)
--- src/main/java/com/devsu/users/domain/jpa/PersonEntity.java
+++ src/main/java/com/devsu/users/domain/jpa/PersonEntity.java
@@ -1,5 +1,31 @@
 package com.devsu.users.domain.jpa;
 
-public class PersonEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+
+@MappedSuperclass
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public abstract class PersonEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+  @Column(nullable = false, length = 255)
+  String name;
+  @Column(length = 50)
+  String gender;
+  Integer age;
+  @Column(length = 100)
+  String identification;
+  @Column(length = 255)
+  String address;
+  @Column(length = 50)
+  String cellphone;
 
 }

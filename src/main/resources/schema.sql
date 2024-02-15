087ea74c5af96a10025358e7bfd95e40bfea25bb
Stalyn-1994 <iguairac@pichincha.com>
2024-02-14 23:15:57 -0500

    [feature/manage-users]script to databases

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+-- Create schema if not exist
+CREATE SCHEMA IF NOT EXISTS users;
+
+-- Create table person if not exist
+CREATE TABLE IF NOT EXISTS users.customer
+(
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
+    identification VARCHAR(255) NOT NULL,
+    name           VARCHAR(255) NOT NULL,
+    gender         VARCHAR(50),
+    age            INT,
+    address        VARCHAR(255),
+    cellphone      VARCHAR(50),
+    password       VARCHAR(255) NOT NULL,
+    state          BOOLEAN DEFAULT FALSE
+);

270fa1276c1d234ef21435c8b239480e3a969f73
Stalyn-1994 <iguairac@pichincha.com>
2024-02-15 18:16:30 -0500

    [feature/manage-users] configurate entities and dto

--- src/main/java/com/devsu/users/domain/jpa/PersonEntity.java
+++ src/main/java/com/devsu/users/domain/jpa/PersonEntity.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 
+@Getter
 @MappedSuperclass
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class PersonEntity {
---
+++ src/main/java/com/devsu/users/service/dto/CustomerDto.java
@@ -0,0 +1,7 @@
+package com.devsu.users.service.dto;
+
+import com.devsu.users.domain.jpa.CustomerEntity;
+
+public class CustomerDto extends CustomerEntity {
+
+}
